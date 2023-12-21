1a867d051f7b4675c0ae52625e4db4648e466e6e
hudson <hava12@naver.com>
2023-12-21 16:05:00 +0000

    openai 초기 설정

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 from dotenv import load_dotenv
+from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+
 load_dotenv()
+
+llm = OpenAI()
+chat_model = ChatOpenAI()
+
+llm.predict('hi!')
+
+chat_model.predict("hi!")
