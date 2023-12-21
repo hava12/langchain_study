be1dabb49679161cccd43dce5a2ef2e158972a53
hudson <hava12@naver.com>
2023-12-21 16:13:37 +0000

    readme 추가

---
+++ README.md
@@ -0,0 +1,13 @@
+# iot_hub_terraform
+
+## Project Overvie
+이 프로젝트는 Langchain을 사용한 기본 파이썬 웹 페이지 구축 프로젝트입니다.
+
+## Prerequisites
+해당 프로젝트를 사용하기 전에 다름 요구 사항을 충족해야 합니다.
+
+- Python 3.11.7
+- OpenAI 계정 및 API Key
+- VS Code
+
+## 초기 환경 세팅
--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-from langchain.llms import OpenAI
-llm = OpenAI()
-result = llm.predict('hi!')
-print(result)
+# from langchain.llms import OpenAI
+# llm = OpenAI()
+# result = llm.predict('hi!')
+# print(result)
 
-# from langchain.chat_models import ChatOpenAI
-# chat_model = ChatOpenAI()
-# chat_model.predict("hi!")
+from langchain.chat_models import ChatOpenAI
+chat_model = ChatOpenAI()
+result = chat_model.predict("hi!")
+print(result)
