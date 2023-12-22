58f4b2eebfd3e0c0fbdbe6e612744146df0264f3
hudson <hava12@naver.com>
2023-12-22 00:03:48 +0000

    commit

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+content = "hi"
+
 # from langchain.llms import OpenAI
 # llm = OpenAI()
-# result = llm.predict('hi!')
+# result = llm.predict(content)
 # print(result)
+
 
 from langchain.chat_models import ChatOpenAI
 chat_model = ChatOpenAI()
-result = chat_model.predict("hi!")
+result = chat_model.predict(content)
 print(result)
