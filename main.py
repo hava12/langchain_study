32955fdf1e505a9b023d649ac6d861ae88249646
hudson <hava12@naver.com>
2023-12-23 13:47:17 +0000

    streamlit 사용

--- main.py
+++ main.py
@@ -2,15 +2,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-content = "hi"
+content = "고양이"
 
 # from langchain.llms import OpenAI
 # llm = OpenAI()
-# result = llm.predict(content)
+# result = llm.predict(content + "에 대한 시를 써줘")
 # print(result)
 
 
 from langchain.chat_models import ChatOpenAI
 chat_model = ChatOpenAI()
-result = chat_model.predict(content)
+result = chat_model.predict(content + "에 대한 시를 써줘")
 print(result)
+
+import streamlit as st
+
+st.title('인공지능 시인')
