a4c7c148cfaf646e2e36e8f48979acb38fc66bbf
hudson <hava12@naver.com>
2023-12-23 14:20:34 +0000

    commit project

--- main.py
+++ main.py
@@ -12,9 +12,20 @@ content = "고양이"
 
 from langchain.chat_models import ChatOpenAI
 chat_model = ChatOpenAI()
-result = chat_model.predict(content + "에 대한 시를 써줘")
-print(result)
+# result = chat_model.predict(content + "에 대한 시를 써줘")
+# print(result)
 
 import streamlit as st
 
 st.title('인공지능 시인')
+title = st.text_input('시의 주제는요?')
+
+if st.button('시 작성 요청하기') : 
+    with st.spinner('시 작성 중...'):
+        content = title
+        result = chat_model.predict(content + "에 대한 시를 써줘")
+        print(result)
+        st.write(result)
+
+# st.write('시의 주제는', title)
+
